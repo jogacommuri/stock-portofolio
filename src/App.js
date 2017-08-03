848080afcf47be17fa378fac2386841dae8e8f3f
Joga Commuri <jogacommuri@yahoo.com>
2017-08-02 22:53:07 -0700

    stock data fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+//import { render } from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import fetchJsonp from 'fetch-jsonp';
-
+import { Button, Form, FormGroup, FormControl} from 'react-bootstrap/lib';
 class App extends Component {
   constructor(props){
     super(props);
@@ -11,7 +12,8 @@ class App extends Component {
       symbol: '',
       price: '',
       quantity: '',
-      date: ''
+      date: '',
+      stockDate: []
     }
     this.updateSymbol = this.updateSymbol.bind(this);
     this.updatePrice = this.updatePrice.bind(this);
@@ -35,8 +37,10 @@ class App extends Component {
   updateDate(e){
     this.setState({date: e.target.value});
   }
-  componentDidMount() {
-    const url = 'http://finance.google.com/finance/info?q=NASDAQ:AAPL,NASDAQ:GOOG,NASDAQ:V'
+  calculateStockDate(symbol) {
+
+    //console.log(symbol)
+    var url = "http://finance.google.com/finance/info?q=NASDAQ:GOOG,NASDAQ:FB,NASDAQ:"+symbol
     fetchJsonp(url,{
       method: 'GET',
       mode: 'no-cors',
@@ -59,22 +63,43 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Stock Portfolio App</h2>
         </div>
-        <div className="stock-form{">
-          <input type="text" value={this.state.symbol} onChange={this.updateSymbol} placholder="Symbol"/>
-          <input type="text" value={this.state.price} onChange={this.updatePrice} placholder="Price"/>
-          <input type="text" value={this.state.quantity} onChange={this.updateQunatity} placholder="Quantity"/>
-          <input type="text" value={this.state.date} onChange={this.updateDate} placholder="Date"/>
-          <h4> {this.state.symbol}</h4>
-          <h4> {this.state.price}</h4>
-          <h4> {this.state.quantity}</h4>
-          <h4> {this.state.date}</h4>
-          {this.state.data.map((stock, index) => 
-            <div key={index} className="test">
-            <h4> {stock.t}</h4>
-            </div>
-            )}
-
-        </div>
+        <div className="container">
+          <div className = "row">
+            <Form inline>
+              <FormGroup controlId="symbol">
+                <FormControl type="text" placeholder="Symbol" value={this.state.symbol} onChange={this.updateSymbol}/>
+              </FormGroup>
+              {' '}
+              <FormGroup controlId="price">
+                <FormControl type="number" placeholder="Price" value={this.state.price} onChange={this.updatePrice}/>
+              </FormGroup>
+              {' '}
+              <FormGroup controlId="quantity">
+                <FormControl type="number" placeholder="Qty" value={this.state.quantity} onChange={this.updateQunatity}/>
+              </FormGroup>
+              {' '}
+              <FormGroup controlId="date">
+                <FormControl type="date" placeholder="DD/MM/YYYY" value={this.state.date} onChange={this.updateDate}/>
+              </FormGroup>
+              {' '}
+              <Button onClick={() => {{this.calculateStockDate(this.state.symbol)}}}>
+                +
+              </Button>
+            </Form>
+            {this.state.data.map((stock, index) => 
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-4 col-md-4" key={index}>
+                    <p> {this.state.symbol} / {this.state.price} * {this.state.quantity} </p>
+                  </div>
+                  <div className="col-lg-4 col-md-4">
+                    <p> {stock.t} / {stock.l} </p>
+                  </div>
+                </div>
+              </div>
+              )}
+          </div>
+        </div>   
       </div>
     );
   }
